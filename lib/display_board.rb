d21f4feac54c09a2d293ac4debbdc0e3e971f129
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-08-20 21:57:45 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,5 +5,5 @@ def display_board(board_array)
   puts "-----------"
   puts " #{board_array[3]} " + "|" + " #{board_array[4]} " + "|" + " #{board_array[5]} "
   puts "-----------"
-  puts " #{board_array[6]} " + "|" + " #{board_array[7]} " + "|" + "   #{board_array[8]} "
+  puts " #{board_array[6]} " + "|" + " #{board_array[7]} " + "|" + " #{board_array[8]} "
 end
